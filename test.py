0e87eda20a6ecb05181b2be7583051e09ab7a6b3
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-16 14:09:21 -0400

    solving the second conditional skills problem

--- test.py
+++ test.py
@@ -3,10 +3,26 @@ print("hello world")
 #Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
 
-var = 9
-if var < 10:
-  print (-1)
-elif var > 10:
-  print(1)
+# var = 9
+# if var < 10:
+#   print (-1)
+# elif var > 10:
+#   print(1)
+# else:
+#   print(0)
+
+#Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+digit1 = 9
+digit2 = 11
+
+# if ((digit1 < 10) AND (digit2 < 10)):
+#   print(-1)
+# else:
+#   print(0)
+
+age = 18
+  
+if ((digit1< 10) and (digit2< 10)):
+    print(-1)
 else:
-  print(0)
+    print(0)
