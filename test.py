3acac368e05c347553fa371e50e7557dd0843049
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-16 13:53:55 -0400

    solving a skills problem in python

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("hello world")
+
+#Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+
+var = 9
+if var < 10:
+  print (-1)
+elif var > 10:
+  print(1)
+else:
+  print(0)
