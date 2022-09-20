c83446028e409d8672fa58779f90bfad82a4df63
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-20 13:55:31 -0400

    question 5 of skills practice

--- test.py
+++ test.py
@@ -21,9 +21,22 @@
 #     print(0)
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
-var = 900
+# var = 900
 
-if (var>9000):
-    print(1)
+# if (var>9000):
+#     print(1)
+# else:
+#     print(-1)
+
+ # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+var = 28
+
+if (var < 10):
+     print(9)
+elif(var < 20):
+     print(19)
+elif(var <30):
+    print(29)
 else:
-    print(-1)
+    print(1)
