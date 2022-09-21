d03bd9672994a60c32dc477d9ca00fa74b821158
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-21 13:25:30 -0400

    added all questions and answered 7 in python

--- test.py
+++ test.py
@@ -30,13 +30,35 @@
 
  # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
-var = 28
+# var = 28
 
-if (var < 10):
-     print(9)
-elif(var < 20):
-     print(19)
-elif(var <30):
-    print(29)
+# if (var < 10):
+#      print(9)
+# elif(var < 20):
+#      print(19)
+# elif(var <30):
+#     print(29)
+# else:
+#     print(1)
+
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+# var1 = 9
+# var2 = 8
+# if (var1>10) or (var2>10):
+#     print(100)
+# else:
+#     print(-100)
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+var = 1
+if (var<0):
+    print(1776)
 else:
-    print(1)
+    print(1979)
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
