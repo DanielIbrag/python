959de525e8fb0c49d51cc7c62ad5253447f93de4
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-22 13:45:25 -0400

    final conditionals practice for python

--- test.py
+++ test.py
@@ -57,15 +57,20 @@
 # else:
 #     print(1979)
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
-var = 99
-if (var == 100):
-    print (100)
-elif (var == 99):
-    print (99)
-else:
-    print (0)
+# var = 99
+# if (var == 100):
+#     print (100)
+# elif (var == 99):
+#     print (99)
+# else:
+#     print (0)
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-
+var1 = -100
+var2 = 100
+if (var1 < 0) and (var2 > 0):
+    print (1)
+else:
+    print (0)
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
